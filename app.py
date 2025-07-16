f36ade5d8b7f7845de3d5832cf44d66e1358efe0
kavinafis <130205641+kavinafis@users.noreply.github.com>
2025-07-17 00:55:41 +0600

    temp database creation app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
