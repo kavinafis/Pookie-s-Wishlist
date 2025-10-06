a2361e22d81e633449c25cdebc4f567996e3588b
kavinafis <130205641+kavinafis@users.noreply.github.com>
2025-10-06 23:19:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+import requests
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
